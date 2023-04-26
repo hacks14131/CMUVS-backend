17ef049a63d0059865c2370b04d30d4ba8e25965
John Clerk Montejo Villarosa <61640087+hacks14131@users.noreply.github.com>
2023-04-26 15:53:53 +0800

    debug

--- routes/candidatePicture.js
+++ routes/candidatePicture.js
@@ -8,12 +8,12 @@ const fileStorageEngine = multer.diskStorage({
     cb(null, './backend/image/');
   },
   filename: (req, file, cb) => {
+    console.log(file);
     cb(null, Date.now() + '-' + file.originalname);
   },
 });
 
 const fileFilter = (req, file, cb) => {
-  console.log(file);
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
