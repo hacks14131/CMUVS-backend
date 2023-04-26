9810904e5af5a073601a85e1e59b25b9ebcf1bc1
John Clerk Montejo Villarosa <61640087+hacks14131@users.noreply.github.com>
2023-04-26 16:34:11 +0800

    new

--- controller/candidatePicture.js
+++ controller/candidatePicture.js
@@ -2,32 +2,33 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const CandidatePicture = require('../models/candidatePicture');
 
-const postCandidatePicture = (req, res, next) => {
+const postCandidatePicture = (req, res) => {
   console.log('controller: ', req.file);
-  const candidatePicture = new CandidatePicture({
-    _id: mongoose.Types.ObjectId(),
-    userID: req.body.userID,
-    profilePicture: {
-      data: fs.readFileSync(`./backend/image/${req.file.filename}`),
-      contentType: 'image/png',
-    },
-  });
-  candidatePicture
-    .save()
-    .then((docs) => {
-      res.status(201).json({
-        _id: docs._id,
-        candidateID: docs.candidateID,
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({
-        'Error Details': {
-          message: error.message,
-          error: error,
-        },
-      });
-    });
+  // const candidatePicture = new CandidatePicture({
+  //   _id: mongoose.Types.ObjectId(),
+  //   userID: req.body.userID,
+  //   profilePicture: {
+  //     data: fs.readFileSync(`./backend/image/${req.file.filename}`),
+  //     contentType: 'image/png',
+  //   },
+  // });
+  // candidatePicture
+  //   .save()
+  //   .then((docs) => {
+  //     res.status(201).json({
+  //       _id: docs._id,
+  //       candidateID: docs.candidateID,
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).json({
+  //       'Error Details': {
+  //         message: error.message,
+  //         error: error,
+  //       },
+  //     });
+  //   });
+  res.status(200).json({ status: 'good' });
 };
 
 const updateCandidateProfilePicture = async (req, res, next) => {
